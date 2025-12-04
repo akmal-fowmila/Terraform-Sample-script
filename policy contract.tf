ccf6a4323003f32290a852fdf8ce7ecee8556a2a
akmal-fowmila <akmal@likemindsconsulting.com>
2025-12-04 19:21:49 +0530

    Simplify registration policy contract attributes
    Removed unused attributes from the registration policy contract.

--- policy contract.tf
+++ policy contract.tf
@@ -5,12 +5,7 @@ resource "pingfederate_authentication_policy_contract" "registration" {
   # Attributes required by your fragment logic
   extended_attributes = [
     { name = "email" },
-    { name = "given_name" },
-    { name = "family_name" },
-    { name = "firstName" },
-    { name = "lastName" },
-    { name = "fullName" },
-    { name = "photo" },
     { name = "username" }
   ]
+
 }
