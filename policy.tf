83c5624574854902aa27ec7cf61c3700ef05196a
akmal-fowmila <akmal@likemindsconsulting.com>
2025-12-04 13:49:26 +0530

    Replaced dynamic policy with HTML form authentication

--- policy.tf
+++ policy.tf
@@ -1,68 +1,71 @@
-resource "pingfederate_authentication_policies" "auth_policies" {
+resource "pingfederate_authentication_policies" "authenticationPolicies" {
   fail_if_no_selection = false
-
+  
   authn_selection_trees = [
     {
-      name        = "Dynamic AuthN Policy"
-      description = "Routes traffic based on 'AuthN_Experience' Extended Property."
-      enabled     = true
-
       root_node = {
         action = {
-          authn_selector_policy_action = {
-            authentication_selector_ref = { id = pingfederate_authentication_selector.authnexp.id }
+          authn_source_policy_action = {
+            authentication_source = {
+              type       = "IDP_ADAPTER"
+              source_ref = {
+                id = pingfederate_idp_adapter.html_form_adapter.id
+              }
+            }
+            attribute_mapping = {
+              attribute_sources = []
+              attribute_contract_fulfillment = {
+                # Map adapter attributes to policy contract attributes
+                "email" = {
+                  source = {
+                    type = "ADAPTER"
+                  }
+                  value = "policy.action"
+                }
+                "given_name" = {
+                  source = {
+                    type = "ADAPTER"
+                  }
+                  value = "username"
+                }
+                "username" = {
+                  source = {
+                    type = "ADAPTER"
+                  }
+                  value = "username"
+                }
+              }
+              issuance_criteria = {
+                conditional_criteria = []
+              }
+            }
           }
         }
-
         children = [
           {
             action = {
-              authn_selector_policy_action = {
-                authentication_selector_ref = { id = pingfederate_authentication_selector.authnexp.id }
-                context = "AuthN_Experience"
+              done_policy_action = {
+                context = "Fail"
               }
             }
-            children = [
-              {
-                action = {
-                  authn_selector_policy_action = {
-                    authentication_selector_ref = { id = pingfederate_authentication_selector.authnexp.id }
-                    context = "Registration"
-                  }
-                }
-                children = [
-                  {
-                    action = {
-                      fragment_policy_action = {
-                        fragment = { id = pingfederate_authentication_policies_fragment.registration_fragment.id }
-                        fragment_mapping = {
-                          attribute_sources = []
-                          attribute_contract_fulfillment = {
-                            "given_name"  = { source = { type = "NO_MAPPING" } }
-                            "family_name" = { source = { type = "NO_MAPPING" } }
-                            "firstName"   = { source = { type = "NO_MAPPING" } }
-                            "lastName"    = { source = { type = "NO_MAPPING" } }
-                            "username"    = { source = { type = "NO_MAPPING" } }
-                            "subject"     = { source = { type = "NO_MAPPING" } }
-                            "email"       = { source = { type = "NO_MAPPING" } }
-                            "fullName"    = { source = { type = "NO_MAPPING" } }
-                            "photo"       = { source = { type = "NO_MAPPING" } }
-                          }
-                          issuance_criteria = { conditional_criteria = [] }
-                        }
-                      }
-                    }
-                    children = [
-                      { action = { done_policy_action = { context = "Fail" } } },
-                      { action = { done_policy_action = { context = "Success" } } }
-                    ]
-                  }
-                ]
+          },
+          {
+            action = {
+              done_policy_action = {
+                context = "Success"
               }
-            ]
+            }
           }
         ]
       }
+      name        = "Simple HTML Form Authentication Policy"
+      description = "Direct authentication using HTML Form IdP Adapter without fragment or selector"
+      enabled     = true
     }
+  ]
+
+  depends_on = [
+    pingfederate_idp_adapter.html_form_adapter,
+    pingfederate_authentication_policy_contract.registration
   ]
 }
